6e0500b2c807c1365f1f71e026e01dd545603fd0
PLK <unrecht.official@gmail.com>
2022-04-13 22:55:55 +0200

    adding stuff

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,9 +2,24 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://zahlenip.de`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-fontawesome-css`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
+      },
+    }
+  ],
 }
 
 export default config
