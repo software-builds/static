3a274d600ad06ceee0863fd8567252f81198b8be
PLK <unrecht.official@gmail.com>
2022-04-14 20:01:00 +0200

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import * as React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 import {
-  faInstagram, faTwitter, faYoutube
+  faInstagram, faLinkedin, faTwitter
 } from '@fortawesome/free-brands-svg-icons';
+
+// redirects
+const redirects = {
+  twitter: "https://twitter.com/lamacode0",
+  instagram: "https://instagram.com/philleonkersting",
+  linkedin: "https://www.linkedin.com/in/phil-leon-kersting-ba6866220/"
+}
 
 // markup
 const IndexPage = () => {
@@ -16,40 +23,50 @@ const IndexPage = () => {
           <div className="hover:scale-105 hover:rotate-6 transition-transform mx-auto border-red-800 border-8 m-10 h-52 w-52 overflow-hidden rounded-full align-middle">
             <img className="w-full" src={'./static/logo.jpg'} alt="" />
           </div>
-
           <h1 className="font-bold">Phil Leon Kersting</h1>
           <hr className="my-4" />
-
-          <ul className="font-mono">
+          <ul className="font-mono text-sm md:text-lg">
             <li>
-              👋 Hi, I’m <a href="https://github.com/lamacode" target="_blank" className="text-blue-300">@lamacode</a> and i’m...
+              👋 Hi, I’m <a href="https://github.com/lamacode" target="_blank" className="text-blue-300">
+                @lamacode
+              </a> and i’m...
             </li>
             <li>👀 interested in programming languages and techonolgies.</li>
             <li>🌱 learning terraforming, java and php constructions.</li>
             <li>💞️ looking to collaborate on huge, small and own projects.</li>
           </ul>
           <hr className="my-4" />
-
           <ul className="flex">
             <li className="flex-1">
-              <a href="https://instagram.zahlenip.de/" target="_blank">
-                <FontAwesomeIcon icon={faInstagram as IconProp} size="2x" className="transition-transform hover:scale-125 hover:text-pink-500" />
+              <a href={redirects.instagram} target="_blank">
+                <FontAwesomeIcon 
+                  icon={faInstagram as IconProp} 
+                  size="2x" 
+                  className="transition-transform hover:scale-125 hover:text-pink-500" 
+                />
               </a>
             </li>
             <li className="text-center flex-1">
-              <a href="https://youtube.zahlenip.de/" target="_blank">
-                <FontAwesomeIcon icon={faYoutube as IconProp} className="transition-transform hover:scale-125 hover:text-red-600" size="2x" />
+              <a href={redirects.linkedin} target="_blank">
+                <FontAwesomeIcon 
+                  icon={faLinkedin as IconProp}
+                  className="transition-transform hover:scale-125 hover:text-blue-500" 
+                  size="2x" 
+                />
               </a>
             </li>
             <li className="text-right flex-1">
-              <a href="https://twitter.zahlenip.de/" target="_blank">
-                <FontAwesomeIcon icon={faTwitter as IconProp} className="transition-transform hover:scale-125 hover:text-blue-500" size="2x" />
+              <a href={redirects.twitter} target="_blank">
+                <FontAwesomeIcon 
+                  icon={faTwitter as IconProp} 
+                  className="transition-transform hover:scale-125 hover:text-blue-500" 
+                  size="2x" 
+                  />
               </a>
             </li>
           </ul>
         </div>
       </div>
-
       <div className="w-full flex h-2">
         <div className="bg-red-600 flex-1"></div>
         <div className="bg-yellow-600 flex-1"></div>
